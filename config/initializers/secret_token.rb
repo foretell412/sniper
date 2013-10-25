2ab9b60263f7931fa939c6e67d14c105eeafbbc7
nguyenthanhtung <tungnt4121991@gmail.com>
2013-10-25 08:15:27 +0000

    Generate a new Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sniper::Application.config.secret_key_base = '973ca83454a02f3e7442cc83fbd5e11d15d164994a94c1bf74f4e7582e9aadc5408c979837961086470cbbf4cf39c24611d4d60034b5cb39eca47ac73d62354b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sniper::Application.config.session_store :cookie_store, key: '_sniper_session'
